1cf4914b0e760f0a2ae4588f0c7096e379586c46
evertonschultz <evertonf.m.schultz98@gmail.com>
2023-01-07 20:37:37 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,38 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import Image from 'next/image'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Agendamento descomplicado | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolixando a aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
+  )
+}
